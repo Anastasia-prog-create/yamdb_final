bd4a472d2bdd69ef7d9b588d709aedd289edcb67
Anastasia Krivosheeva <anastasia.krivosheeva.97@gmail.com>
2022-04-21 15:53:25 +0300

    Изменен DEFAULT_PERMISSION_CLASSES

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -117,7 +117,7 @@ EMPTY_VALUE_DISPLAY = '-пусто-'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
